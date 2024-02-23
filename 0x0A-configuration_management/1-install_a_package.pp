142c95c613a7b9a8f577d696d412e8e8ebc4a014
Maroa2002 <mgm.engineeringtie847@gmail.com>
2024-02-24 00:05:06 +0300

    install flask from pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,12 @@
 # installing flask from pip3
 
 package {'flask':
-  ensure   => 2.1.0,
+  ensure   => '2.1.0',
+  provider => 'pip3'
+}
+
+package {'Werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3'
 }
 
