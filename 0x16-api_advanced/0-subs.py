7bc4d30abef1760ca86cfc2ddf4d60d37b3c08fb
Maroa2002 <mgm.engineeringtie847@gmail.com>
2024-05-11 17:00:14 +0300

    changes to task 0;

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,21 +2,26 @@
 """
 Query Reddit API for number of subscribers for a given subreddit
 """
-import requests
+from json import loads
+from requests import get
 
 
 def number_of_subscribers(subreddit):
+    """ecursive function that queries the Reddit API and returns a list
+    containing the titles of all hot articles for a given subreddit. If no
+    results are found for the given subreddit, the function should return None
     """
-        return number of subscribers for a given subreddit
-        return 0 if invalid subreddit given
-    """
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {
+        'User-Agent':
+        'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
+        Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
+    }
+    response = get(url, headers=headers)
+    reddits = response.json()
 
-    headers = requests.utils.default_headers()
-    headers.update({'User-Agent': 'My User Agent 1.0'})
-
-    r = requests.get(url, headers=headers).json()
-    subscribers = r.get('data', {}).get('subscribers')
-    if not subscribers:
+    try:
+        subscribers = reddits.get('data').get('subscribers')
+        return int(subscribers)
+    except:
         return 0
-    return subscribers
