fb78a7686479d7005e69d29acbc87f490a2721ec
Maroa2002 <mgm.engineeringtie847@gmail.com>
2024-05-13 17:50:57 +0300

    task 0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,10 @@
+# fix request limit at nginx stack
+
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+}
+-> exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
