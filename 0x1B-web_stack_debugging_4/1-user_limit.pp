daa9aa82916936e9563f5b6dccaba0439c2bc0cb
Maroa2002 <mgm.engineeringtie847@gmail.com>
2024-05-13 17:53:07 +0300

    task 1

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,10 @@
+# fix limit file at hbton user
+
+exec { 'fix_limit_hbton_user':
+  command => 'sed -i "/holberton hard/s/5/10000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+exec { 'increase_soft_file':
+  command => 'sed -i "/holberton soft/s/4/20000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
